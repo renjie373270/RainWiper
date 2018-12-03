27c2fa472ebe44976e7ede520abf346edd9da689
RenJie <renjie37370@163.com>
2018-12-03 14:14:00 +0800

    忽略部分文件

--- user/IR.c
+++ user/IR.c
@@ -6,25 +6,18 @@
 
 uint32_t pulseCount = 0;
 
-void sendPulses(int count) {
-    int i = 0;
-    while (i++ < count) {
-        PWM_ON();
-        delay_ms(1);
-        PWM_OFF();
-        delay_ms(1);
-    }
-}
+
 
 void IR_Handle() {
-    pulseCount = 0;
-    sendPulses(50);
     PWM_ON();
+    delay_ms(100);
     if(pulseCount > 5) {
         pulseCount = 0;
-        LED_Flash();
-        LED_Flash();
+//        LED_Flash();
     }
+    pulseCount = 0;
+    PWM_OFF();
+    delay_ms(100);
 }
 
 void EXTI15_10_IRQHandler(void) {
--- user/main.c
+++ user/main.c
@@ -6,7 +6,7 @@ void AllDeviceInit(void)
 	SystemInit();
 	UserSysTick_Config(72000000);
 	LED_Init();
-    TIM1_PWM_CH1_Config(72000000, 38000, 0.5);
+    TIM1_PWM_CH1_Config(72000000, 38000, 0.1);
 	IR_EXTI_Init();
 }
 
